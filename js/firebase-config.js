0d7cd86b872bff41d2a75c6f08da0783cd51d923
MARIPALLI MALLESH <inomallesh@gmail.com>
2026-08-10 10:34:28 +0530

    Refactor firebase config to use environment variables
    Updated firebase configuration to use environment variables for sensitive data.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,14 +7,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
+const env = (typeof window !== "undefined" && window.__ENV__) || globalThis.process?.env || {};
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD6VsVa-s23mEn5s2TkqPdyNPCYR4D51Q0",
-  authDomain: "livelyhub-b75a0.firebaseapp.com",
-  databaseURL: "https://livelyhub-b75a0-default-rtdb.firebaseio.com",
-  projectId: "livelyhub-b75a0",
-  storageBucket: "livelyhub-b75a0.firebasestorage.app",
-  messagingSenderId: "885842871186",
-  appId: "1:885842871186:web:52174cea19bbbebe60774b",
+  apiKey: env.FIREBASE_API_KEY ,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: env.FIREBASE_DATABASE_URL ,
+  projectId: env.FIREBASE_PROJECT_ID ,
+  storageBucket: env.FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID ,
+  appId: env.FIREBASE_APP_ID ,
 };
 
 const app = initializeApp(firebaseConfig);
